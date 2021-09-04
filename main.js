81f5259e5327cad7765dbfd22ca76d9538b88a8e
Carlos Daniel <dev.cdfaria@gmail.com>
2021-09-04 14:54:47 -0300

    Refactoring

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
-const container = document.querySelector('.container')
-const unsplashURL = 'https://source.unsplash.com/random/'
-const rows = 10
+const RandomImage = {
+  container: document.querySelector('.container'),
+  unsplashURL: 'https://source.unsplash.com/random/',
+  rows: 10,
 
-for(let i = 0; i < rows * 3; i++) {
-  const img = document.createElement('img')
-  img.src = `${unsplashURL}${getRandomSize()}`
-  img.alt = `Image ${i + 1}`
-  container.appendChild(img)
+  createImages() {
+    for(let i = 0; i < RandomImage.rows * 3; i++) {
+      const img = document.createElement('img')
+      img.src = `${RandomImage.unsplashURL}${RandomImage.getRandomSize()}`
+      img.alt = `Image ${i + 1}`
+      RandomImage.container.appendChild(img)
+    }
+  },
+  
+  getRandomSize: () => `${RandomImage.getRandomNumber()}x${RandomImage.getRandomNumber()}`,
+  getRandomNumber: () => Math.floor(Math.random() * 10) + 300,
 }
 
-function getRandomSize() {
-  return `${getRandomNumber()}x${getRandomNumber()}`
-}
-
-function getRandomNumber() {
-  return Math.floor(Math.random() * 10) + 300
-}
+RandomImage.createImages()
